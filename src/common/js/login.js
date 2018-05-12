c9b7c86c87af6c7af4817732435a9c86cb74db11
benza613 <benza.alv@hotmail.com>
2018-05-12 17:41:59 +0530

    login js axios make api

--- src/common/js/login.js
+++ src/common/js/login.js
@@ -10,13 +10,6 @@ app.controller('login', function ($rootScope, $scope, $http, $uibModal, ab, c, u
     };
 
     $scope.vm.submit = () => {
-        var config = {
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-        };
-
-        // axios.post('http://otwlfrt4.azurewebsites.net/api/ocean/sp_MA_GetJobs', {
-        //       auth_token: 'F068B6AF-BC7E-45E7-A18C-A47B6C8A4E7B'
-        //  }, config)
 
         const params = new URLSearchParams();
         params.append('auth_token', 'F068B6AF-BC7E-45E7-A18C-A47B6C8A4E7B');
@@ -29,8 +22,7 @@ app.controller('login', function ($rootScope, $scope, $http, $uibModal, ab, c, u
         })
             .then(response => {
                 console.log(response)
-                console.log(response.data.url);
-                console.log(response.data.explanation);
+
             })
             .catch(error => {
                 console.log(error);
